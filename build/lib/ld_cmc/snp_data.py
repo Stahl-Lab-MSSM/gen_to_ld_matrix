e898cf9028197fcaa9a571aa5b5e9ccd4efad0ac
James Boocock <smilefreak@gmx.com>
2016-07-15 14:38:03 -0400

    Deleted a bunch of files

--- build/lib/ld_cmc/snp_data.py
+++
@@ -1,9 +0,0 @@
-
-class SnpData(object):
-    def __init__(self,chrom,snps,pos,a1,a2,maf):
-        self.chrom=chrom
-        self.snpid=snpid
-        self.pos=pos
-        self.a1=a1
-        self.a2=a2
-        self.maf=maf
